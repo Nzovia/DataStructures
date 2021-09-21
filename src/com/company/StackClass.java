1c5352190916e840cec097de7374ca9580ff6f82
ADMIN <nickiemaundu19@gmail.com>
2021-09-21 12:12:40 +0300

    StackClass of the iterator interface in the java collection framework, push operation

---
+++ src/com/company/StackClass.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class StackClass {
+    public static void main(String[] args) {
+        //the stack class of the collection framework is used to implement the stack datastracture
+        //different operations can be achieved and are based LIFO(last in first out), and operations include
+        //pop, push , empty, search, peek
+        Stack<String> s = new Stack<>();
+        //adding data into the stack
+        s.push("Nicholas");
+        s.push("Maundu");
+        s.push("Developer");
+
+        //create an iterator for  the stack
+        Iterator<String> iterator= s.iterator();
+
+        //displaying the list of items in the iterator
+        while (iterator.hasNext()){
+            System.out.println(iterator.next()+ " ");
+        }
+    }
+
+}
